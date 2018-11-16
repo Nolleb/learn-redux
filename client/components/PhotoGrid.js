4b018a53a8b854f9a80c3ed5892e8540b83ec193
Roger Bellon <rbellongronnier@gmail.com>
2018-11-16 22:53:58 +0100

    add files to project

---
+++ client/components/PhotoGrid.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const PhotoGrid = React.createClass({
+    render(){
+        return(
+            <div>
+                <h1>
+                    I am the photo grid
+                </h1>
+            </div>
+        );
+    }
+});
+
+export default PhotoGrid;
---
+++ client/components/Single.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Single = React.createClass({
+    render(){
+        return(
+            <div>
+                <h1>
+                    I am the single
+                </h1>
+            </div>
+        );
+    }
+});
+
+export default Single;
